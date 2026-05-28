354503a32c62dbe4cdddb2a2541834c7fa9a36fc
Rakshit <rakshitverma034@gmail.com>
2026-05-28 11:24:46 +0000

    feat: Setup Supabase public and storage client

---
+++ app/api/upload/route.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+
+// A private key used to bypass RLS or upload files in the supabase db
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+// Client for backend storage operations
+const supabaseStorage = createClient(url, serviceKey || anonKey);
+
+// Public supabase client
+const supabase = createClient(url, anonKey);

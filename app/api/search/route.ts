114c29d522442754abaa6ca3bd2983f969673805
Rakshit <rakshitverma034@gmail.com>
2026-05-30 11:01:10 +0000

    feat: Convert input search query to vector embeddigns using openai

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,7 @@
 import OpenAI from 'openai';
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
+import { request } from 'http';
 
 // Extract environment variables from system
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -9,3 +10,26 @@ const anonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 // Initialize client services
 const supabase = createClient(url, anonKey);
 const openai = new OpenAI();
+
+export async function POST(req: Request) {
+	try {
+		const { query } = await req.json();
+
+		if (!query || query.trim == '') {
+			return NextResponse.json(
+				{ error: 'Query text is required' },
+				{ status: 400 }
+			);
+		}
+
+		const embeddingResponse = await openai.embeddings.create({
+			input: query,
+			model: 'text-embedding-3-small'
+		});
+
+		const queryEmbeddings = embeddingResponse.data[0].embedding;
+	}
+	catch (err: any) {
+			console.log ("Bhaari Error");
+	}
+}
